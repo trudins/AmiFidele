44577e0d0f039e21fe29fc16bd5bde468a19115e
apotre <victordievi@gmail.com>
2023-05-25 11:55:19 +0100

    ajout logger

--- src/main/java/com/dievi/principal/AmiFidele.java
+++ src/main/java/com/dievi/principal/AmiFidele.java
@@ -1,9 +1,15 @@
 package com.dievi.principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AmiFidele {
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(AmiFidele.class);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		LOGGER.info("Hello, (Logging) World!");
 		
 		AmiFidele am=new AmiFidele();
 	}
