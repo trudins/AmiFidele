a9523c1751365245004862c93ca7b02b0b58d0d0
apotre <victordievi@gmail.com>
2023-05-25 09:02:48 +0100

    creation classe principale AmiFidele

---
+++ src/main/java/com/dievi/principal/AmiFidele.java
@@ -0,0 +1,11 @@
+package com.dievi.principal;
+
+public class AmiFidele {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AmiFidele am=new AmiFidele();
+	}
+
+}
