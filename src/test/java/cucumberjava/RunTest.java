8763c52fe53bb6d9dace226e4cd303024300be10
apotre <victordievi@gmail.com>
2023-05-31 04:05:00 +0100

    ajout de runTest

--- src/test/java/cucumberjava/RunTest.java
+++ src/test/java/cucumberjava/RunTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;;
 
 @RunWith(Cucumber.class) 
-@CucumberOptions(plugin = {"pretty"},
+@CucumberOptions(plugin = {"pretty","json:target/cucumber/cucumber.json","html:target/CucumberTestReport.html"},
 features= {"src/test/resources/cucumberjava"},glue= {"cucumberjava"})
 
 public class RunTest {
